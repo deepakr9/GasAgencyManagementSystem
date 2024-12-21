3850ada2e05876dfec92146fbbddf87d158909bf
deepakr9 <130228097+deepakr9@users.noreply.github.com>
2024-12-21 14:43:55 +0530

    file added

--- file.py
+++ file.py
@@ -1,15 +1,29 @@
 
-# with open("file.txt", 'a') as f:
-#   for i in range(5):
-#     res = input()
-#     f.write(res+'\n')
+# # with open("file.txt", 'a') as f:
+# #   for i in range(5):
+# #     res = input()
+# #     f.write(res+'\n')
 
 
+# # f.close()
+
+# # Read methods
+
+# with open('file.txt', 'r') as f:
+#     res = f.read()
+#     with open('file4.txt', 'a') as f1:
+#         f1.write(res)
+#     f1.close()
 # f.close()
 
-# Read methods
+# with open('file4.txt', 'r') as f3:
+#     result = f3.read()
+#     print(result)
+# f3.close()
 
-with open('file.txt', 'r') as f:
-    res = f.readlines()
-    print(res)
-f.close()
+with open('image.jpg', 'rb') as f1:
+    res = f1.read(95000)
+    with open('car.jpg', 'wb') as f2:
+        f2.write(res)
+        f2.close()
+    f1.close()
